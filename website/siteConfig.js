56fe31eb21967a6f5590936526fa2482250ac7e5
llccrr <loic1.renaud@hotmail.fr>
2018-07-17 15:51:46 +0200

    config to deploy

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -31,7 +31,7 @@ const siteConfig = {
 
   // Used for publishing and more
   projectName: 'overuse',
-  organizationName: 'viseo',
+  organizationName: 'NGRP',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
@@ -93,7 +93,7 @@ const siteConfig = {
 
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
-  repoUrl: 'https://github.com/NGRP/capitalization-overuse',
+  repoUrl: 'https://github.com/NGRP/overuse',
 };
 
 module.exports = siteConfig;
